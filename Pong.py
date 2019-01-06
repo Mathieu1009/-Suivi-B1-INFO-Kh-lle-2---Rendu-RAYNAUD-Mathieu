299e070a4cd87080db8888424325f4bebb502075
Mathieu1009 <43410006+Mathieu1009@users.noreply.github.com>
2019-01-06 22:01:59 +0100

    Update Pong.py

--- Pong.py
+++ Pong.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-# -*- coding: UTF-8 -*-
 
 from tkinter import *
 from random import randrange
@@ -10,12 +8,13 @@ def jouer():
 
 		global dx, dy
 		canevas.move(balle, dx, dy)
-		Fenetre.after(30, deplacement_balle)
+		Fenetre.after(50, deplacement_balle)
 		x0_balle,y0_balle,x1_balle,y1_balle = canevas.coords(balle)
 		x0_1,y0_1,x1_1,y1_1 = canevas.coords(raquette1)
 		x0_2,y0_2,x1_2,y1_2 = canevas.coords(raquette2)
 
-		#Test de la colision à la raquette
+		#Colision à la raquette
+
 		if x0_balle <= 0:
 			canevas.coords(balle,338, 238, 362, 262)
 			canevas.pack_forget()
@@ -40,28 +39,32 @@ def jouer():
 		x0_2,y0_2,x1_2,y1_2 = canevas.coords(raquette2)
 		touche = event.keysym
 		if touche == "Up" and y0_2 > 0:
-			y0_2=y0_2-10
-			y1_2=y1_2-10
+			y0_2=y0_2-30
+			y1_2=y1_2-30
 			canevas.coords(raquette2,x0_2,y0_2,x1_2,y1_2)
+
 		elif touche == "Down" and y1_2 < 500:
-			y0_2=y0_2+10
-			y1_2=y1_2+10
+			y0_2=y0_2+30
+			y1_2=y1_2+30
 			canevas.coords(raquette2,x0_2,y0_2,x1_2,y1_2)
 
 		if touche == "z" and y0_1 > 0:
-			y0_1=y0_1-10
-			y1_1=y1_1-10
+			y0_1=y0_1-30
+			y1_1=y1_1-30
 			canevas.coords(raquette1,x0_1,y0_1,x1_1,y1_1)
+
 		elif touche == "s" and y1_1 < 500:
-			y0_1=y0_1+10
-			y1_1=y1_1+10
+			y0_1=y0_1+30
+			y1_1=y1_1+30
 			canevas.coords(raquette1,x0_1,y0_1,x1_1,y1_1)
 
-	#Fenetre graphique
-	Fenetre.title("Jeu du Pong")
-	canevas = Canvas(Fenetre,width=700,height=500,bg='#000000',cursor="none")
+	#Fenetre de jeu
 
-	#Initialisation du terrain
+	Fenetre.title("Jeu du Pong")
+	canevas = Canvas(Fenetre,width=700,height=500,bg='blue',cursor="none")
+
+	#Initialisation du terrain de jeu
+
 	canevas.create_line(350, 0, 350, 500, fill='white')
 	Bord1 = canevas.create_rectangle(0, 3, 4, 497, fill='white')
 	Bord2 = canevas.create_rectangle(696, 3, 700, 497, fill='white')
@@ -69,9 +72,10 @@ def jouer():
 	Bord4 = canevas.create_rectangle(0, 496, 700, 500, fill='white')
 
 	#Initialisation des Modules
-	balle = canevas.create_oval((338, 238, 362, 262),fill='white')
-	raquette1 = canevas.create_rectangle(20,200,30,300,fill='white')
-	raquette2 = canevas.create_rectangle(670,200,680,300,fill='white')
+
+	balle = canevas.create_oval((338, 238, 362, 262),fill='yellow')
+	raquette1 = canevas.create_rectangle(20,200,30,300,fill='red')
+	raquette2 = canevas.create_rectangle(670,200,680,300,fill='black')
 
 
 	canevas.focus_set()
@@ -86,17 +90,20 @@ def jouer():
 def menu():
 
 	#Initialisation du menu et des boutons
-	Fenetre.title('Menu')
-	Debut = Button(Fenetre, text='Débuter', command=lambda:[Debut.grid_forget(),Quitter.grid_forget(),jouer()])
+
+	Fenetre.title('PONG')
+	Debut = Button(Fenetre, text='Nouvelle partie', command=lambda:[Debut.grid_forget(),Quitter.grid_forget(),jouer()])
 	Debut.grid(row=1, column=1)
-	Quitter = Button(Fenetre, text='Quitter', command=Fenetre.destroy)
+	Quitter = Button(Fenetre, text='Quitter le jeu', command=Fenetre.destroy)
 	Quitter.grid(row=1, column=2)
 
 	Fenetre.mainloop()
 	Fenetre.destroy()
 
 Fenetre = Tk()
-#Direction de la Balle aléatoire
+
+#Direction de la Balle aléatoire au lancement du pong
+
 dx = randrange(-8, 8)
 dy = randrange(-8, 8)
 
